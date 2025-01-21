4bd6c41ff786802282fde121190b225d275738a9
Spoxbz <minerolgestbz@gmail.com>
2025-01-20 22:23:17 -0500

    cmvoid

--- fhe-app/src/backendTwo/model/model.absent_patient.ts
+++
@@ -1,7 +0,0 @@
-export interface AbsentPatients {
-  id: number;
-  patient_id: number;
-  appointment_id: number;
-  marked_by: number;
-  created_at: Date;
-}
--- fhe-app/src/backendTwo/model/model.attended_patients.ts
+++
@@ -1,8 +0,0 @@
-export interface AttendedPatients {
-  id: number;
-  patient_id: number;
-  appointment_id: number;
-  module_id: string;
-  attended_by: number;
-  attended_at: Date;
-}
