c005db1e60f6464c7d53541a558904c9b36e25e8
Spoxbz <minerolgestbz@gmail.com>
2025-03-05 10:46:08 -0500

    cm73
    modificacion de la interfaz de ficha medica:
    - accordiones colapsables con propiedad expanded para evitar scroll extra del usuario

--- fhe-app/src/components/MEDICALOFFICE/records/Top.tsx
+++ fhe-app/src/components/MEDICALOFFICE/records/Top.tsx
@@ -1,8 +1,15 @@
 import { GridExpandMoreIcon } from "@mui/x-data-grid";
 import "../../../CSS/medical_record/medicalrecordpage.css";
 import { Accordion, AccordionDetails, AccordionSummary, TextField, Typography } from "@mui/material";
+import React from "react";
 
 export default function Top() {
+  const [expandee, setExpandee] = React.useState<string | false>("panel1");
+
+  // Control del manejo de la expansion de cada panel del acordeon
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpandee(isExpanded ? panel : false);
+  };
   const accordionSummaryStyle = {
     "&.Mui-expanded": {
       backgroundColor: "rgba(198, 205, 230, 0.5)",
@@ -11,10 +18,10 @@ export default function Top() {
   return (
     <>
       <div className="mr-top-container">
-        <Accordion>
+        <Accordion expanded={expandee === "panel1"} onChange={handleChange("panel1")}>
           <AccordionSummary
             expandIcon={<GridExpandMoreIcon />}
-            aria-controls="panel1-content"
+            aria-controls="panel1bh-content"
             id="panel1-header"
             sx={accordionSummaryStyle}
           >
@@ -22,30 +29,30 @@ export default function Top() {
           </AccordionSummary>
           <AccordionDetails>
             <div className="mr-top-info-turn">
-              <TextField id="outlined-basic" label="Turno" variant="outlined" />
-              <TextField id="outlined-basic" label="Especialidad" variant="outlined" />
-              <TextField id="outlined-basic" label="Médico" variant="outlined" />
+              <TextField id="filled-basic" label="Turno" variant="filled" />
+              <TextField id="filled-basic" label="Especialidad" variant="filled" />
+              <TextField id="filled-basic" label="Médico" variant="filled" />
               <TextField
-                id="outlined-basic"
+                id="filled-basic"
                 label="Fecha"
-                variant="outlined"
+                variant="filled"
                 type="date"
                 slotProps={{ inputLabel: { shrink: true } }}
               />
               <TextField
-                id="outlined-basic"
+                id="filled-basic"
                 label="F. Previa"
-                variant="outlined"
+                variant="filled"
                 type="date"
                 slotProps={{ inputLabel: { shrink: true } }}
               />
             </div>
           </AccordionDetails>
         </Accordion>
-        <Accordion>
+        <Accordion expanded={expandee === "panel2"} onChange={handleChange("panel2")}>
           <AccordionSummary
             expandIcon={<GridExpandMoreIcon />}
-            aria-controls="panel1-content"
+            aria-controls="panel2bh-content"
             id="panel1-header"
             sx={accordionSummaryStyle}
           >
@@ -53,32 +60,32 @@ export default function Top() {
           </AccordionSummary>
           <AccordionDetails>
             <div className="mr-top-info-patient">
-              <TextField id="outlined-basic" label="Paciente" variant="outlined" />
-              <TextField id="outlined-basic" label="C.I" variant="outlined" />
-              <TextField id="outlined-basic" label="H.C" variant="outlined" />
-              <TextField id="outlined-basic" label="Edad" variant="outlined" />
-              <TextField id="outlined-basic" label="Genero" variant="outlined" />
+              <TextField id="filled-basic" label="Paciente" variant="filled" />
+              <TextField id="filled-basic" label="C.I" variant="filled" />
+              <TextField id="filled-basic" label="H.C" variant="filled" />
+              <TextField id="filled-basic" label="Edad" variant="filled" />
+              <TextField id="filled-basic" label="Genero" variant="filled" />
             </div>
           </AccordionDetails>
         </Accordion>{" "}
-        <Accordion>
+        <Accordion expanded={expandee === "panel3"} onChange={handleChange("panel3")}>
           <AccordionSummary
             expandIcon={<GridExpandMoreIcon />}
-            aria-controls="panel1-content"
+            aria-controls="panel3bh-content"
             id="panel1-header"
             sx={accordionSummaryStyle}
           >
             <Typography component="span">Signos Vitales</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <div className="mr-top-info-patient">
-              <TextField id="outlined-basic" label="Temperatura" variant="outlined" />
-              <TextField id="outlined-basic" label="Peso (Kg)" variant="outlined" />
-              <TextField id="outlined-basic" label="Talla (cm)" variant="outlined" />
-              <TextField id="outlined-basic" label="Saturación" variant="outlined" />
-              <TextField id="outlined-basic" label="Tensión Art." variant="outlined" />
-              <TextField id="outlined-basic" label="Pulso" variant="outlined" />
-              <TextField id="outlined-basic" label="Frecuencia Res." variant="outlined" />
+            <div className="mr-top-info-signs-patient">
+              <TextField id="filled-basic" label="Temperatura" variant="filled" />
+              <TextField id="filled-basic" label="Peso (Kg)" variant="filled" />
+              <TextField id="filled-basic" label="Talla (cm)" variant="filled" />
+              <TextField id="filled-basic" label="Saturación" variant="filled" />
+              <TextField id="filled-basic" label="Tensión Art." variant="filled" />
+              <TextField id="filled-basic" label="Pulso" variant="filled" />
+              <TextField id="filled-basic" label="Frecuencia Res." variant="filled" />
             </div>
           </AccordionDetails>
         </Accordion>
